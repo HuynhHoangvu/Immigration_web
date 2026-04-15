21e07c7c3e3ab1229519d08f203c69396fad4406
Huynh Hoang Vu <hhoangvu001@gmail.com>
2026-04-15 15:41:28 +0700

    add test eng

--- fly-labour-frontend/src/themes/fly-labour/parts/Header.tsx
+++ fly-labour-frontend/src/themes/fly-labour/parts/Header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
           : ""
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+      <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-10">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Logo */}
           <Link to="/" className="flex items-center shrink-0">
